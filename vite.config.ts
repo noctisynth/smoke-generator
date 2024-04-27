743d37c0efcc202659e2d563447528480dfe2fa2
苏向夜 <fu050409@163.com>
2024-04-27 21:21:10 +0800

    chore: setup unocss and primevue

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
