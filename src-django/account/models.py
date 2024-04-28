2d3915ea433f39803ac0c15e4b0db48f1cdc2143
FreetBash <3141495167@qq.com>
2024-04-28 12:50:33 +0800

    feat(account): 已完成用户模块

---
+++ src-django/account/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+#   - [ ] 账号（唯一）
+#   - [ ] 密码
+#   - [ ] 电话
+#   - [ ] 邮箱
+#   - [ ] 个人/企业身份选择
+#   - [ ] 头像上传
+
+
+class UserAccount(models.Model):
+    STATUS_CHOICES = (
+        ("个人", "个人"),
+        ("企业", "企业"),
+    )
+    username = models.CharField(unique=True, verbose_name="账号", max_length=39)
+    password = models.CharField(max_length=39, verbose_name="密码")
+    phone = models.CharField(max_length=39, verbose_name="电话")
+    email = models.EmailField(max_length=39 * 2, verbose_name="邮箱")
+    status = models.CharField(
+        max_length=39, choices=STATUS_CHOICES, default="个人", verbose_name="身份"
+    )
+    # Image字段保存图片
+    avatar = models.ImageField(
+        upload_to="avatar", verbose_name="头像", default="123.jpg", null=True
+    )
+
+
+class UserToken(models.Model):
+    token = models.CharField(
+        max_length=39 * 2, verbose_name="Token", null=False, unique=True
+    )
+    account = models.ForeignKey(
+        UserAccount, on_delete=models.CASCADE, null=False, related_name="token"
+    )
---
+++ src-django/account/views.py
@@ -0,0 +1,153 @@
+from django.http import JsonResponse, HttpRequest
+from .models import UserAccount, UserToken
+from django.contrib.auth.hashers import make_password
+from util import selectData, verifyToken
+
+
+def add(request: HttpRequest):
+    """
+    {
+        "username"  :"bash",
+        "password"  :"bash123",
+        "phone"     :"12345677890",
+        "email"     :"123@asd.com"
+    }
+    """
+
+    data = selectData(request)
+
+    username: str = data.get("username")
+    password: str = data.get("password")
+    phone: str = data.get("phone")
+    email: str = data.get("email")
+
+    if not all([username, password, phone, email]):
+        return JsonResponse({"status": 402, "message": "参数错误"})
+
+    if UserAccount.objects.filter(username=username).exists():
+        return JsonResponse({"status": 400, "message": "用户已存在"})
+
+    ua = UserAccount()
+    ua.username = username
+    ua.password = password
+    ua.phone = phone
+    ua.email = email
+    ua.save()
+
+    return JsonResponse({"status": 200, "message": "用户创建成功"})
+
+
+def login(request: HttpRequest):
+    """
+    {
+        "username"  :"bash",
+        "password"  :"bash123",
+        "token"     :"123123"
+    }
+
+     若无则留空
+    """
+    data = selectData(request)
+
+    token: str = data.get("token")
+    ua = verifyToken(token)
+
+    if ua:
+        return JsonResponse({"status": 201, "message": "用户已登陆"})
+
+    username: str = data.get("username")
+    password: str = data.get("password")
+
+    if not all([username, password]):
+        return JsonResponse({"status": 402, "message": "参数错误"})
+
+    q = UserAccount.objects.filter(username=username)
+    if q.count() == 0:
+        return JsonResponse({"status": 402, "message": "用户不存在"})
+
+    if password != q[0].password:
+        return JsonResponse({"status": 403, "message": "密码错误"})
+
+    # create token
+    token = make_password(request.session.session_key)
+    ut = UserToken()
+    ut.account = q[0]
+    ut.token = token
+    ut.save()
+
+    return JsonResponse({"status": 200, "message": "登录成功", "token": token})
+
+
+def logout(request: HttpRequest):
+    data = selectData(request)
+    token = data.get("token")
+    ua = verifyToken(token)
+
+    if ua:
+        t = UserToken.objects.filter(account=ua)
+        t.delete()
+
+    return JsonResponse({"status": 200, "message": "退出成功"})
+
+
+def profile(request: HttpRequest):
+    """
+    {
+      "token": "!XqFbIUKQRG9rUCEgUOmf3nlWWi8iSwIZaiwtmIZK"
+    }
+    """
+    data = selectData(request)
+    token: str = data.get("token")
+    ua = verifyToken(token)
+
+    if not ua:
+        return JsonResponse({"status": 403, "message": "用户未登录"})
+
+    res_data = {
+        "phone": ua.phone,
+        "email": ua.email,
+        "status": ua.status,
+        "avatar": ua.avatar.url,
+    }
+
+    return JsonResponse(res_data)
+
+
+def update(request: HttpRequest):
+    """
+    {
+        "token":"123",
+        "password":"123456",
+        "phone":"1233456",
+        "email":"123@dfvw.qwe",
+        "status":"企业",
+    }
+    不需要更新的直接留空
+    """
+    data = selectData(request)
+
+    token: str = data.get("token")
+    ua = verifyToken(token)
+
+    if not ua:
+        return JsonResponse({"status": 403, "message": "用户未登录"})
+
+    password: str = data.get("password")
+    phone: str = data.get("phone")
+    email: str = data.get("email")
+    status: str = data.get("status")
+    avatar = request.FILES.get("avatar")
+
+    if password:
+        ua.password = password
+    if phone:
+        ua.phone = phone
+    if email:
+        ua.email = email
+    if status:
+        ua.status = status
+    if avatar:
+        ua.avatar = avatar
+
+    ua.save()
+    return JsonResponse({"status": 200})
