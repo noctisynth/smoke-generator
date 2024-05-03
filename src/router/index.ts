b678ed3e69f2371a07e40c6ccdf61cfe6bbf7ebd
苏向夜 <fu050409@163.com>
2024-05-03 15:30:48 +0800

    feat(register): add register page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {
+  createRouter,
+  createWebHashHistory,
+  type RouteRecordRaw,
+} from "vue-router";
 
+// 动态导入views目录下的所有vue文件
+const views = import.meta.glob([
+  "../views/**/*.vue",
+  "../views/**/index.vue",
+  "../views/**/\\[*\\].vue",
+]);
+
+// 路由配置
+const routes: RouteRecordRaw[] = Object.entries(views).map(
+  ([filePath, component]) => {
+    // 处理路由路径
+    let path = filePath.replace(/^\.\.\/views\//, "");
+    path = path.replace(/^(.*)\/?index\.vue$/, "$1");
+    path = path.replace(/\[(\w+)\]\.vue$/, ":$1");
+    path = path.replace(/\.vue$/, "");
+    path = "/" + path;
+
+    return {
+      path,
+      name: filePath.replace(/^\.\.\/views\//, ""),
+      component,
+    } satisfies RouteRecordRaw;
+  }
+);
+
+// 打印路由配置
+console.table(routes);
+
+// 创建路由实例
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [...routes],
+});
 
-export default router
+// 导出路由实例
+export default router;
