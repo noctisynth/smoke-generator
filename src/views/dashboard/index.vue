616ff73527f55ea0d0101419698540c74f76671e
苏向夜 <fu050409@163.com>
2024-05-10 18:25:32 +0800

    refactor(announcement): refactor announcement to outer

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,36 +1,13 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-import items from '@/scripts/items';
-
-// 右上角菜单
-const menu = ref();
-
-// 公告内容
-const announcement = ref<string>('');
-const showAnnouncement = ref<boolean>(true);
-async function fetchAnnouncement() {
-    announcement.value = '公告内容';
-}
 
 onMounted(async () => {
-    // 页面渲染完成后获取公告内容
-    await fetchAnnouncement();
 })
 </script>
 
 <template>
-    <div :class="['fixed inset-x-0 bottom-0 p-4', (showAnnouncement ? '' : 'hidden')]">
-        <div class="relative flex items-center justify-between rounded-lg bg-indigo-600 px-4 py-1 text-white shadow-lg">
-            <p class="text-sm font-medium">
-                {{ announcement }}
-            </p>
-
-            <Button @click="showAnnouncement = false" icon="pi pi-times text-coolGray hover:!text-white"
-                class="hover:!bg-indigo-600 hover:!text-white" plain text>
-            </Button>
-        </div>
-    </div>
     <div class="flex flex-col w-full h-full">
+        <Announcement></Announcement>
         <TopBar></TopBar>
         <div class="flex flex-row w-full p-1rem gap-4 h-full bg-#f8fafc">
             <SidePanel></SidePanel>
